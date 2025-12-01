e549931a60f59752574c81f37ff68476b2964e8a
Solonarv <nstamm@gmx.de>
2025-12-01 18:25:19 +0100

    solve day 1

--- haskell/solutions/Day01.hs
+++ haskell/solutions/Day01.hs
@@ -1,39 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day01 where
 
 import AOC.Solution
 
 import ParsingPrelude
-import Util
+import Util hiding (invert)
+import Data.Finite
 
-import Control.Lens
-import Data.Map (Map)
-import Data.Map.Strict qualified as Map
-
-solution :: Solution ([Int], [Int]) [Int] Int
+solution :: Solution [Integer] Int Integer
 solution = Solution
-  { decodeInput = fmap (over both sort . unzip) $ ((,) <$> decimal <*> (noeol *> decimal)) `sepBy` eol
-  , solveA = Solver
-    { solve = Just . uncurry (zipWith (\x y -> abs (x-y)))
-    , display = show . sum
+  { decodeInput = parseInstruction `sepBy` space1
+  , solveA = defSolver
+    { solve = Just . countHits (==0) . scanl (+) 50 . map (modulo @100)
     }
   , solveB = defSolver
-    { solve = Just . getSum . Map.foldMapWithKey (\k n -> Sum (k*n)) . uncurry countSimilarity
+    { solve = Just . zeroPasses 100 50
     }
   , tests =
-    [ unlines
-      [ "3   4"
-      , "4   3"
-      , "2   5"
-      , "1   3"
-      , "3   9"
-      , "3   3"
-      ] :=> [(PartA, "11"), (PartB, "31")]
-
+    [ "L68 L30 R48 L5 R60 L55 L1 L99 R14 L82"
+      :=> [(PartA, "3"), (PartB, "6")]
+    , "R1000 L700" :=> [(PartB, "17")]
     ]
   }
 
--- Note: problem statement is unclear on how to handle duplicates in left list.
--- Assuming they are to be counted multiple times.
-countSimilarity :: [Int] -> [Int] -> Map Int Int
-countSimilarity needles hay = Map.intersectionWith (*) (freq needles) (freq hay)
-  where freq = getFreqs . toFreqMap
+parseInstruction :: Parser Integer
+parseInstruction = ((negate <$ "L") <|> (id <$ "R")) <*> decimal
+
+zeroPasses :: Integer -> Integer -> [Integer] -> Integer
+zeroPasses size start = fst . foldl step (0, start)
+  where
+    invert x | x == 0 = 0 | otherwise = size-x
+    step (acc, pos) i = let
+      pos' = (i + pos) `mod` size
+      passes
+        | i >= 0 = (i + pos) `div` size
+        | otherwise = (invert pos - i) `div` size
+      in -- traceShow ((acc, pos), i, pos', passes)
+        (acc + passes, pos')
